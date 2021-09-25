2af35e2fd537409d96f7f8ab871f4b1f015b3838
Akahiko Yukimura <akahikoyukimura39@gmail.com>
2021-09-25 17:54:46 +0100

    home page

---
+++ src/data/InfoData.js
@@ -0,0 +1,26 @@
+import ImageOne from '../images/design_1.jpg';
+import ImageTwo from '../images/house_5.jpg';
+
+export const InfoData={
+    heading:'Explore our beautiful homes',
+    paragrapheOne:
+    'Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr vLorem is sum dolor amet constzr',
+    paragrapheTwo:
+    'Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr vLorem is sum dolor amet constzr',
+    buttonLabel:'View Homes',
+    image:ImageOne,
+    reverse:false,
+    delay:100
+};
+
+export const InfoDataTwo={
+    heading:'Modern Designs',
+    paragrapheOne:
+    'Lorem is suconstzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr vLorem is sum dolor amet constzr',
+    paragrapheTwo:
+    'Lorem is is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr Lorem is sum dolor amet constzr vLorem is sum dolor amet constzr',
+    buttonLabel:'View Homes',
+    image:ImageTwo,
+    reverse:true,
+    delay:300
+};
---
+++ src/data/SliderData.js
@@ -0,0 +1,74 @@
+import ImageOne from '../images/house_1.jpg';
+import ImageTwo from '../images/house_2.jpg';
+import ImageThree from '../images/house_3.jpg';
+import ImageFour from '../images/house_4.jpg';
+
+export const SliderData=[
+    {
+        title:'Luxury Villa in Bali, Indonesia',
+        price:'$4,280,000',
+        path:'/homes',
+        label:'View Home',
+        image:ImageOne ,
+        alt:'House'
+    },
+    {
+        title:'House in San Jose, Costa Rica',
+        price:'$2,280,000',
+        path:'/homes',
+        label:'View Home',
+        image:ImageTwo ,
+        alt:'House'
+    },
+    {
+        title:'House in Calabassas, California',
+        price:'$8,420,000',
+        path:'/homes',
+        label:'View Home',
+        image:ImageThree ,
+        alt:'House'
+    },
+    {
+        title:'House in Houston, Texas',
+        price:'$6,660,000',
+        path:'/homes',
+        label:'View Home',
+        image:ImageFour ,
+        alt:'House'
+    },
+];
+
+// export const SliderDataTwo=[
+//     {
+//         title:'House in Las Vegas',
+//         price:'$48,000',
+//         path:'/homes',
+//         label:'View Home',
+//         image:ImageFive ,
+//         alt:'House'
+//     },
+//     {
+//         title:'House Texas',
+//         price:'$3,460,000',
+//         path:'/homes',
+//         label:'View Home',
+//         image:ImageSix ,
+//         alt:'House'
+//     },
+//     {
+//         title:'House in Alajuela',
+//         price:'$5,420,000',
+//         path:'/homes',
+//         label:'View Home',
+//         image:ImageSeven ,
+//         alt:'House'
+//     },
+//     {
+//         title:'House in Mouston',
+//         price:'$6,660,000',
+//         path:'/homes',
+//         label:'View Home',
+//         image:ImageFour ,
+//         alt:'House'
+//     },
+// ];
